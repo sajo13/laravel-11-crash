0dde83b1683e49f6f52a8ec0265042d7858a654b
sajo13 <sajosunny007@gmail.com>
2025-01-15 13:19:05 +0530

    Style the active navigation link using Laravel's Request object

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -19,9 +19,9 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <a href="/" class=" rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                                <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                             </div>
                         </div>
                     </div>
